be5e70366e289729a4830df359f8b583876082c9
rdmclin2 <rdmclin2@163.com>
2016-06-10 22:09:43 +0800

    add eslint and eslintify api_apk.js

--- api/v1/api_apk.js
+++ api/v1/api_apk.js
@@ -1,194 +1,183 @@
-var ApkModel = require('../../models/model_apk');
-var RecordModel = require('../../models/model_record');
-var TestcaseModel = require('../../models/model_testcase');
-var eventproxy= require('eventproxy');
-var _  = require('lodash');
+const ApkModel = require('../../models/model_apk')
+const RecordModel = require('../../models/model_record')
+const TestcaseModel = require('../../models/model_testcase')
+const Eventproxy = require('eventproxy')
+const _ = require('lodash')
 
-module.exports.create = function(req, res, next) {
+module.exports.create = (req, res, next) => {
+  const apk = new ApkModel()
+  apk.name = req.body.name
+  apk.version = req.body.version
+  apk.package_name = req.body.package_name
+  apk.activity_name = req.body.activity_name
 
-  var apk= new ApkModel();
-  apk.name = req.body.name;
-  apk.version = req.body.version;
-  apk.package_name = req.body.package_name;
-  apk.activity_name = req.body.activity_name;
-
-  apk.save(function(err, apk) {
+  apk.save((err, data) => {
     if (err) {
-      console.log(err);
-      return next(err);
+      return next(err)
+    }
+    return res.status(200).json(data)
+  })
+}
+
+module.exports.update = (req, res, next) => {
+  const apkId = req.params.id
+  const name = req.body.name
+  const version = req.body.version
+  const packageName = req.body.package_name
+  const activityName = req.body.activity_name
+
+  ApkModel.findOne({ _id: apkId }).exec((err, apk) => {
+    apk.name = name
+    apk.version = version
+    apk.package_name = packageName
+    apk.activity_name = activityName
+
+    apk.save((error) => {
+      if (error) return next(error)
+      return res.status(200).send('save success')
+    })
+  })
+}
+
+module.exports.remove = (req, res, next) => {
+  const ep = new Eventproxy()
+  ep.fail(next)
+
+  const apkId = req.params.id
+
+  ep.all('records', 'testcases', 'apk', (records, testcase, apk) => {
+    res.status(200).json(apk)
+  })
+
+  TestcaseModel.find({ apk_id: apkId }).remove(ep.done('testcases'))
+  RecordModel.find({ apk_id: apkId }).remove(ep.done('records'))
+  ApkModel.findOneAndRemove({ _id: apkId }).exec(ep.done('apk'))
+}
+
+
+module.exports.addBlacklist = (req, res, next) => {
+  const apkId = req.params.id
+  const item = req.body.item.trim()
+
+  if (!apkId || ! item) {
+    return res.status(422).send('error')
+  }
+
+  ApkModel.findOne({ _id: apkId }).exec((err, apk) => {
+    const index = apk.blacklist.indexOf(item)
+
+    if (index !== -1) {
+      res.status(200).send('save success')
     } else {
-      return res.status(200).json(apk);
+      apk.blacklist.push(item)
+      apk.save((error) => {
+        if (error) return next(error)
+        return res.status(200).send('save success')
+      })
     }
-  });
-};
-
-module.exports.update= function(req, res, next) {
-  var apk_id = req.params.id;
-  var name = req.body.name;
-  var version = req.body.version;
-  var package_name = req.body.package_name;
-  var activity_name = req.body.activity_name;
-
-  ApkModel.findOne({_id:apk_id}).exec(function(err,apk){
-    apk.name = name;
-    apk.version = version;
-    apk.package_name = package_name;
-    apk.activity_name = activity_name;
-
-    apk.save(function (err) {
-      if (err) return next(err);
-      res.status(200).send("save success");
-    });
-  });
-};
-
-module.exports.remove = function(req, res, next) {
-  var ep = new eventproxy();
-  ep.fail(next);
-  var apk_id= req.params.id;
-  console.log("apk_id");
-
-  ep.all('records','testcases','apk',function(records,testcase,apk){
-    res.status(200).json(apk);
-  });
-
-  TestcaseModel.find({apk_id: apk_id}).remove(ep.done('testcases'));
-  RecordModel.find({apk_id: apk_id}).remove(ep.done('records'));
-  ApkModel.findOneAndRemove({_id:apk_id}).exec(ep.done('apk'));
-
-};
-
-
-module.exports.addBlacklist = function(req, res, next) {
-  var apk_id= req.params.id;
-  var item = req.body.item.trim();
-
-  if(!apk_id || ! item){
-    return res.status(422).send("error");
-  }
-
-  ApkModel.findOne({_id:apk_id}).exec(function(err,apk){
-    var index = apk.blacklist.indexOf(item);
-
-    if(index !== -1){
-      res.status(200).send("save success");
-    }else {
-      apk.blacklist.push(item);
-
-      apk.save(function (err) {
-        if (err) return next(err);
-        res.status(200).send("save success");
-      });
-    }
-  });
+  })
 }
 
-module.exports.removeBlacklist = function(req, res, next) {
-  var apk_id= req.params.id;
-  var item = req.body.item.trim();
+module.exports.removeBlacklist = (req, res, next) => {
+  const apkId = req.params.id
+  const item = req.body.item.trim()
 
-  if(!apk_id || ! item){
-    return res.status(422).send("error");
+  if (!apkId || ! item) {
+    return res.status(422).send('error')
   }
 
-  ApkModel.findOne({_id:apk_id}).exec(function(err,apk){
-    var index = apk.blacklist.indexOf(item);
-    if(index === -1 ){
-      res.status(404).send("not found");
-    }else {
-      apk.blacklist.splice(index,1);
+  ApkModel.findOne({ _id: apkId }).exec((err, apk) => {
+    const index = apk.blacklist.indexOf(item)
+    if (index === -1) {
+      res.status(404).send('not found')
+    } else {
+      apk.blacklist.splice(index, 1)
 
-      apk.save(function (err) {
-        if (err) return next(err);
-        res.status(200).send("delete success");
-      });
+      apk.save((error) => {
+        if (error) return next(error)
+        return res.status(200).send('delete success')
+      })
     }
-  });
+  })
+}
+
+module.exports.removeActpath = (req, res, next) => {
+  const apkId = req.params.id
+  const actName = req.params.actpath.trim()
+
+  ApkModel.findOne({ _id: apkId }).exec((err, apk) => {
+    _.remove(apk.actpaths, (actPath) => {
+      return actPath.activity === actName
+    })
+
+    apk.save((error) => {
+      if (error) return next(error)
+      return res.status(200).send('save success')
+    })
+  })
+}
+
+module.exports.getactions = (req, res, next) => {
+  const apkId = req.params.id
+  const actName = req.params.actpath.trim()
+
+  ApkModel.findOne({ _id: apkId }).exec((err, apk) => {
+    if (err) return next(err)
+
+    const actPath = _.find(apk.actpaths, { activity: actName })
+    if (actPath) {
+      const actions = actPath.actions.join('\n') || ''
+      return res.status(200).json(actions)
+    }
+    return res.status(404).send('no action path')
+  })
 }
 
 
+module.exports.testcases = (req, res, next) => {
+  const apkId = req.params.id
 
-module.exports.removeActpath = function(req, res, next) {
-  var apk_id= req.params.id;
-  var act_name = req.params.actpath.trim();
+  TestcaseModel.find({ apk_id: apkId, type: 'complete', isFinish: true })
+  .exec((err, testcases) => {
+    if (err) return next(err)
 
-  ApkModel.findOne({_id:apk_id}).exec(function(err,apk){
-    _.remove(apk.actpaths,function(actPath) {
-      return actPath.activity === act_name
-    });
-
-    //console.log(JSON.stringify(apk.actpaths));
-
-    apk.save(function (err) {
-      if (err) return next(err);
-      res.status(200).send("save success");
-    });
-  });
-};
-
-module.exports.getactions = function(req, res, next) {
-  var apk_id= req.params.id;
-  var act_name = req.params.actpath.trim();
-  //console.log(apk_id);
-  //console.log(act_name);
-
-  ApkModel.findOne({_id:apk_id}).exec(function(err,apk){
-    if (err) return next(err);
-
-    var actPath = _.find(apk.actpaths, {'activity': act_name});
-    if(actPath){
-      var actions = actPath.actions.join("\n") || "";
-      res.status(200).json(actions);
-    }else{
-      res.status(404).send("no action path");
-    }
-  });
-};
-
-
-module.exports.testcases = function(req, res, next) {
-  var apk_id = req.params.id;
-
-  TestcaseModel.find({apk_id: apk_id, type: 'complete',isFinish: true}).exec(function(err,testcases){
-    if(err ) return next(err);
-
-    res.status(200).json(testcases);
-  });
+    return res.status(200).json(testcases)
+  })
 }
 
-module.exports.actpath= function(req, res, next) {
-  var package_name = req.params.package;
-  var activity_name = req.body.activity_name;
-  var actions = req.body.actions;
-  if(!activity_name || ! actions){
-    return res.status(422).send("error");
+module.exports.actpath = (req, res, next) => {
+  const packageName = req.params.package
+  const activityName = req.body.activity_name
+  let actions = req.body.actions
+
+  if (!activityName || ! actions) {
+    return res.status(422).send('error')
   }
 
-  actions = actions.trim().split("\n");
+  actions = actions.trim().split('\n')
 
-  ApkModel.findOne({package_name:package_name}).exec(function(err,apk){
-    var actPaths = apk.actpaths;
-    var actPath = _.find(actPaths, {'activity': activity_name});
-    if(!actPath){
-      var newActPath = {
-        "activity" : activity_name,
-        "actions" : actions
+  ApkModel.findOne({ package_name: packageName }).exec((err, apk) => {
+    const actPaths = apk.actpaths
+    const actPath = _.find(actPaths, { activity: activityName })
+    if (!actPath) {
+      const newActPath = {
+        activity: activityName,
+        actions,
       }
-      apk.actpaths.push(newActPath);
-      apk.save(function (err) {
-        if (err) return next(err);
-        res.status(200).send("insert success");
-      });
-    }else if(actPath.actions.length > actions.length){
-      actPath.actions = actions;
-      apk.save(function (err) {
-        if (err) return next(err);
-        res.status(200).send("update shorter");
-      });
-    }else {
-      res.status(200).send("not change");
+      apk.actpaths.push(newActPath)
+      apk.save((error) => {
+        if (error) return next(error)
+        return res.status(200).send('insert success')
+      })
+    } else if (actPath.actions.length > actions.length) {
+      actPath.actions = actions
+      apk.save((error) => {
+        if (error) return next(error)
+        return res.status(200).send('update shorter')
+      })
+    } else {
+      res.status(200).send('not change')
     }
-
-  });
-};
+  })
+}
 
